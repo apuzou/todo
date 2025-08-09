631a12cd5e6b6a7f0a449c509a4fa7772b63a2a3
maeda seita <seita93@gmail.com>
2025-08-10 08:08:50 +0900

    検索機能実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $todos = Todo::with('category')->get();
+        $categories = Category::all();
+        return view('index', compact('todos', 'categories'));
+    }
+
+    public function store(TodoRequest $request)
+    {
+        $todo = $request->only(['content', 'category_id']);
+        Todo::create($todo);
+        return redirect('/')->with('message', 'Todoを作成しました');
+    }
+
+    public function update(TodoRequest $request)
+    {
+        $todo = $request->only(['content']);
+        Todo::find($request->id)->update($todo);
+        return redirect('/')->with('message', 'Todoを更新しました');
+    }
+
+    public function destroy(Request $request)
+    {
+        Todo::find($request->id)->delete();
+        return redirect('/')->with('message', 'Todoを削除しました');
+    }
+
+    public function search(Request $request)
+    {
+        $todos = Todo::with('category')->CategorySearch($request->category_id)->KeywordSearch($request->keyword)->get();
+        $categories = Category::all();
+        return view('index', compact('todos', 'categories'));
     }
 }
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,9 +13,18 @@
 <body>
   <header class="header">
     <div class="header__inner">
-      <a class="header__logo" href="/">
-        Todo
-      </a>
+      <div class="header-utilities">
+        <a class="header__logo" href="/">
+          Todo
+        </a>
+        <nav>
+          <ul class="header-nav">
+            <li class="header-nav__item">
+              <a class="header-nav__link" href="/categories">カテゴリー一覧</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 
@@ -22,4 +32,5 @@
     @yield('content')
   </main>
 </body>
+
 </html>
